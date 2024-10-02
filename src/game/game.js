3a4a616c16cfc6e08fc877c1c6aa0ececbc8f081
Haley Sorensen <sorensenhaley@gmail.com>
2024-10-02 14:13:05 -0600

    Feat: Create player class for boards, and attacks

--- src/game/game.js
+++ src/game/game.js
@@ -69,13 +69,37 @@ class Gameboard {
   receiveAttack(coordinate) {
     let index = findIndex(this.oceanGrid, coordinate);
     if (this.oceanInfo[index].shipExist !== null) {
+      //Increase Ship hit count, check if ship sunk
       this.oceanInfo[index].shipExist.hit();
       this.oceanInfo[index].shipExist.isSunk();
-      return (this.oceanInfo[index].hit = true);
+      //Update Board info to show index is a hit
+      this.oceanInfo[index].hit = true;
+      return ('hit');
     } else {
-      return (this.oceanInfo[index].guess = true);
+      //Update Board info to show index is a miss
+      this.oceanInfo[index].guess = true
+      return ('miss');
     }
   }
 }
 
-export { Ship, Gameboard };
+class Player {
+  constructor(){
+    //Player sees their own ships 
+    this.oceanBoard = new Gameboard();
+    //Players sees the hit/miss they've made on the enemy
+    this.targetBoard = new Gameboard();
+  }
+
+  giveAttack(enemy, coordinate){
+    let index = findIndex(this.oceanBoard.oceanGrid, coordinate);
+
+    if(enemy.oceanBoard.receiveAttack(coordinate) === 'hit'){
+      this.targetBoard.oceanInfo[index].hit = true;
+    } else {
+      this.targetBoard.oceanInfo[index].guess = true;
+    }
+  }
+}
+
+export { Ship, Gameboard, Player };
