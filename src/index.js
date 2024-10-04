346a94e9f029c294ab0b83df2523bb5f0dea9182
Haley Sorensen <sorensenhaley@gmail.com>
2024-10-04 12:01:29 -0600

    Feat: Display two boards on screen

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 import html from "./index.html";
 import "./style.css";
+import { Player } from "./game/game.js";
+
+function displayBoard(boardInfo, container) {
+  for (let i = 0; i < boardInfo.length; i++) {
+    let square = document.createElement("div");
+    square.className = "square";
+    square.dataset.coordinate = boardInfo[i].coordinate;
+
+    container.appendChild(square);
+  }
+}
+
+const userBoard = document.querySelector("#user_field");
+const targetBoard = document.querySelector("#target_field");
+
+let userPlayer = new Player();
+
+displayBoard(userPlayer.oceanBoard.boardInfo, userBoard);
+displayBoard(userPlayer.targetBoard.boardInfo, targetBoard);
+
+userBoard.addEventListener("click", (e) => {
+  console.log(e.target);
+});
+
+targetBoard.addEventListener("click", (e) => {
+  console.log(e.target);
+});
