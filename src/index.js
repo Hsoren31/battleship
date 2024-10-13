bdb13cb40dc9a3096632508b9032d093f65f4b55
Haley Sorensen <sorensenhaley@gmail.com>
2024-10-13 12:15:51 -0600

    Feat: Display color squares to represent info

--- src/index.js
+++ src/index.js
@@ -2,28 +2,42 @@ import html from "./index.html";
 import "./style.css";
 import { Player } from "./game/game.js";
 
+const gameContainer  = document.querySelector('#game_container')
+
 function displayBoard(boardInfo, container) {
   for (let i = 0; i < boardInfo.length; i++) {
     let square = document.createElement("div");
     square.className = "square";
     square.dataset.coordinate = boardInfo[i].coordinate;
 
+    if(boardInfo[i].hit !== false){
+      square.classList.add('square_hit')
+    }
+
+    if(boardInfo[i].hasShip !== null){
+      square.classList.add('square_ship')
+    }
+
+    if(boardInfo[i].guess !== false){
+      square.classList.add('square_guess')
+    }
+
     container.appendChild(square);
   }
 }
 
-const userBoard = document.querySelector("#user_field");
-const targetBoard = document.querySelector("#target_field");
+function displayBoards(player){
+  const userBoard = document.querySelector("#user_field");
+  const targetBoard = document.querySelector("#target_field");
+
+  displayBoard(player.oceanBoard.boardInfo, userBoard);
+  displayBoard(player.targetBoard.boardInfo, targetBoard);
+}
 
 let userPlayer = new Player();
 
-displayBoard(userPlayer.oceanBoard.boardInfo, userBoard);
-displayBoard(userPlayer.targetBoard.boardInfo, targetBoard);
+displayBoards(userPlayer);
 
-userBoard.addEventListener("click", (e) => {
-  console.log(e.target);
-});
-
-targetBoard.addEventListener("click", (e) => {
-  console.log(e.target);
-});
+gameContainer.addEventListener('click', (e) => {
+  console.log(e.target)
+})
