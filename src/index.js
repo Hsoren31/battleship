fe153cb0656f1e6f27f8bd883e15b457fae94b3c
Haley Sorensen <sorensenhaley@gmail.com>
2024-12-04 15:46:33 -0700

    feat: add event listeners to drive the game

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import html from "./index.html";
 import "./style.css";
-import { Player } from "./game/game";
+import { Controller } from "./game/game";
 
-const targetBoardContainer  = document.querySelector('#target_board');
+const startGameBtn = document.querySelector("#start_game");
+const gameContainer = document.querySelector("#game_container");
+const placeFleetBtn = document.querySelector("#place_ships");
+const targetBoardContainer = document.querySelector("#target_board");
 
-targetBoardContainer.addEventListener('click', (e) => {
-  if(e.target.className.toLowerCase() === 'square'){
-    let attack = e.target.dataset.coordinate;
-    testing1.giveAttack(testing2, attack)
-  }
-})
+startGameBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  startGameBtn.style.display = 'none'
+  //Set controller
+  let controller = new Controller();
+  //show the game board
+  gameContainer.style.display = "flex";
+  controller.player1.displayBoards();
+
+  //Place Fleet of Ships for both players
+  placeFleetBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    controller.setShips();
+    placeFleetBtn.style.display = "none";
+  });
+
+  //Attack
+  targetBoardContainer.addEventListener("click", (e) => {
+    if (e.target.className.toLowerCase() === "square") {
+      let attack = e.target.dataset.coordinate;
+      controller.playRound(attack)
+    }
+  });
+});
